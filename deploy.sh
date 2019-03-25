7617559d314a6391a1d60d1c1c4aa766282aa8f3
Chikanaga Tomoyuki <t-chikanaga@groovenauts.jp>
2019-03-25 23:13:32 +0900

    Add favicon.

--- deploy.sh
+++ deploy.sh
@@ -11,6 +11,8 @@ rm -rf dist
 mkdir -p dist/image_trainer
 mkdir -p dist/sound_trainer
 
+cp assets/favicon.ico dist/
+
 gsutil -m cp -R gs://${BUCKET}/${IMAGE_TRAINER_DIR}/\* dist/image_trainer/
 gsutil -m cp -R gs://${BUCKET}/${SOUND_TRAINER_DIR}/\* dist/sound_trainer/
 
