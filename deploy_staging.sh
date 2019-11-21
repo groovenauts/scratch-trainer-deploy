68d2826e49426330a39f086da2f34d960d651f9b
Chikanaga Tomoyuki <t-chikanaga@groovenauts.jp>
2019-11-21 16:45:22 +0900

    Add staging deployments.

---
+++ deploy_staging.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+set -e
+
+BUCKET=ai-techpark-assets
+
+IMAGE_TRAINER_DIR=image_detection_training
+SOUND_TRAINER_DIR=sound_detection_training
+
+rm -rf dist
+mkdir -p dist/image_trainer
+mkdir -p dist/sound_trainer
+
+cp assets/favicon.ico dist/
+
+gsutil -m cp -R gs://${BUCKET}/${IMAGE_TRAINER_DIR}/\* dist/image_trainer/
+gsutil -m cp -R gs://${BUCKET}/${SOUND_TRAINER_DIR}/\* dist/sound_trainer/
+
+firebase deploy --only hosting:ai-for-edu-staging
+
